935b45549fca3393ced07794ad7f219061050601
อรุณี ประกอบผล <s6002041520191@Macmini-213-18.local>
2019-04-04 17:00:13 +0700

    login with api

---
+++ components/Login.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import axios from 'axios';
+
+class Login extends Component {
+    constructor(){
+        super()
+        this.state={
+            email: '',
+            password: ''
+        }
+    }
+    onChangeEmail(e){
+        console.log('onChangeEmail', e)
+        this.setState({email: e})
+    }
+    onChangePassword(e){
+        console.log('onChangePassword', e)
+        this.setState({password: e})
+    }
+    onPress(){
+        console.log(this.state)
+        const url = 'http://128.199.240.120:9999/api/auth/login'
+        axios.post(url, this.state)
+            .then(Response => {
+                console.log('token' , response.data.date.token)
+            })
+    }
+    render (){
+        return (
+        <View>
+                <TextInput
+                    style = {{height: 40, fontSize: 20 }}
+                    placeholder = "Email"
+                    value={this.state.email}
+                    onChangeText={this.onChangeEmail.bind(this)}
+                />
+
+                <TextInput
+                    secureTextEntry
+                    style = {{height: 40, fontSize: 20 }}
+                    placeholder = "Password"
+                    value={this.state.password}
+                    onChangeText={this.onChangePassword.bind(this)}
+                />
+
+                <Button 
+                    title = "Login"
+                    onPress={this.onPress.bind(this)}
+                />
+        </View>
+        );
+    }
+}
+
+export default Login;
